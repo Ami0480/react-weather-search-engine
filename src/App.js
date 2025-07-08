053cdf07c88edfea71716598aa2e60fff3d96c8e
Ami0480 <amifuku80@gmail.com>
2025-07-08 14:09:13 +0800

    Added boostrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/Ami0480" target="_blank">
-          Ami Fukuyama
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Ami0480/react-weather-search-engine"
-          target="_blank"
-        >
-          open-sourced on Github
-        </a>{" "}
-        and is{" "}
-        <a href="https://golden-pixie-c5be61.netlify.app" target="_blank">
-          hosted on Netlify
-        </a>
-        .
-      </footer>
+      <div clasName="container">
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/Ami0480" target="_blank">
+            Ami Fukuyama
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Ami0480/react-weather-search-engine"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>{" "}
+          and is{" "}
+          <a href="https://golden-pixie-c5be61.netlify.app" target="_blank">
+            hosted on Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
