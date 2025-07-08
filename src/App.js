38cd78259cf3f884a3292066e92660d97fbf1243
Ami0480 <amifuku80@gmail.com>
2025-07-08 13:55:21 +0800

    Linked github and netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Ami0480" target="_blank">
+          Ami Fukuyama
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Ami0480/react-weather-search-engine"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>{" "}
+        and is{" "}
+        <a href="https://golden-pixie-c5be61.netlify.app" target="_blank">
+          hosted on Netlify
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
