b63d466b8670acc3cbc5749618091b8d1417d940
Ami0480 <amifuku80@gmail.com>
2025-07-09 14:59:08 +0800

    Added weather icon

---
+++ src/WeatherIcon.js
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactAnimatedWeather from "react-animated-weather";
+
+export default function WeatherIcon(props) {
+  const codeMapping = {
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "scattered-clouds-day": "PARTLY_CLOUDY_DAY",
+    "scattered-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "broken-clouds-day": "CLOUDY",
+    "broken-clouds-night": "CLOUDY",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "FOG",
+    "mist-night": "FOG",
+  };
+
+  if (!props.code) {
+    console.warn("WeatherIcon: missing or invalid props.code");
+    return null;
+  }
+
+  const icon = codeMapping[props.code] || "CLOUDY";
+
+  return (
+    <ReactAnimatedWeather icon={icon} color="black" size={50} animate={true} />
+  );
+}
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
-import "./WeatherInfo.css";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherInfo";
 
 export default function WeatherInfor(props) {
   return (
@@ -11,20 +12,24 @@ export default function WeatherInfor(props) {
           <ul>
             <li>
               <FormattedDate date={props.data.date} />
-              <div className="text-uppercase">{props.data.description}</div>
+              <div className="text-uppercase mt-3">
+                {props.data.description}
+              </div>
             </li>
             <li>
               Humidity:{props.data.humidity}%, Wind:{props.data.wind}km/h
             </li>
           </ul>
         </div>
-        <div className="col-6 ">
-          <img src={props.data.iconUrl} className="float-left" />
 
+        <div className="col-6 d-flex align-items-center">
+          <span className="me-2 mt-1">
+            <WeatherIcon code={props.data.icon} />
+          </span>
           <span className="temperature">
             {Math.round(props.data.temperature)}
           </span>
-          <span className="unit">°C</span>
+          <span className="unit ms-1 ">°C</span>
         </div>
       </div>
     </div>
